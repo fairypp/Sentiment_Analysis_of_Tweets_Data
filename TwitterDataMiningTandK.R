dfdf1104a1499356499b18967df0fe9c0f8ed89f
fairypp <fairypp@qq.com>
2017-01-30 00:11:36 -0600

    Update TwitterDataMiningTandK.R

--- TwitterDataMiningTandK.R
+++ TwitterDataMiningTandK.R
@@ -17,7 +17,7 @@ fetchTweets <- function(query, num, lang) # return a data frame
   # Transform tweets list into a data frame
   rawTweets <- twListToDF(rawTweets)
 }
-
+#natural language processing, use package "tm" or "RTextTools". Here I use "tm"
 rawTextProcess <- function(my_text,num_remove=FALSE,keywords_remove=FALSE, keywords, output)
 #if output=0 return character; if output=1 return DocumentTermMatrix with columns as each word's frequency
 {
@@ -26,14 +26,19 @@ rawTextProcess <- function(my_text,num_remove=FALSE,keywords_remove=FALSE, keywo
   docs <- Corpus(VectorSource(my_text))
   #remove weird characters appear in the text
   docs = tm_map(docs, str_replace_all,"[^[A-Za-z0-9]]", " ")
-  
+  #remove extra space
   docs <- tm_map(docs, stripWhitespace)
+  #remove punctuation
   docs <- tm_map(docs, removePunctuation)
   if(num_remove)
+    #remove numbers if num_remove is true
     docs <- tm_map(docs, removeNumbers)
+  #convert all characters to lower case
   docs <- tm_map(docs, tolower)
+  #remove all stopwords in English
   docs <- tm_map(docs, removeWords, stopwords("english"))
   if(keywords_remove)
+  #remove user defined keywords
     docs <- tm_map(docs, removeWords, keywords)   
   library(SnowballC)   
   docs <- tm_map(docs, stemDocument) 
@@ -41,29 +46,52 @@ rawTextProcess <- function(my_text,num_remove=FALSE,keywords_remove=FALSE, keywo
   
   if(output==0)
   {
+    #output as dataframe
     df <- data.frame(text = sapply(docs, as.character), stringsAsFactors = FALSE)
     return (df)
   }
   if(output==1)
   {
+    #output as document term matrix
     dtm <- DocumentTermMatrix(docs)   
     dtm <- as.matrix(dtm)
+    #draw word cloud for top high frequent words
+    freq <- sort(colSums(dtm), decreasing=TRUE)   
+    library(wordcloud)
+    set.seed(142)   
+    wordcloud(names(freq), freq, min.freq=5, scale=c(5, .1), colors=brewer.pal(6, "Dark2"))
     return (dtm)
   }
 }
+    
+# Run Twitter Search and fetch data from twitter. Format is searchTwitter("Search Terms", n=100, lang="en", geocode="lat,lng", also accepts since and until)
+rawTweets <- searchTwitter("taylor swift kanye west", n=5000, lang="en", since="2016-06-20")
+# Transform tweets list into a data frame
+rawTweets <- twListToDF(rawTweets)
+#collect tweets data
+tweets.text <- rawTextProcess(rawTweets$text,num_remove=TRUE,keywords_remove=TRUE,keywords = c("https", "taylor","swift","kanye","west","justin","bieber","kim","kardashian"))
 
-rawTweetsKPPos <- fetchTweets(query = "to:katyperry:)", num = 500, lang="en")
-KPPosMat <- rawTextProcess(rawTweetsKPPos$text,num_remove=FALSE,keywords_remove=TRUE,keywords = c("katy","perry","katyperry"), output=0)
+## try to fetch replies of taylorswift13's latest tweets
+# Run Twitter Search and fetch data from twitter. Format is searchTwitter("Search Terms", n=100, lang="en", geocode="lat,lng", also accepts since and until)
+rawTweetsReply <- searchTwitter("to:taylorswift13", n=2000, lang="en", since="2016-07-17")
+# Transform tweets list into a data frame
+rawTweetsReply <- twListToDF(rawTweetsReply)
+rawTextProcess(rawTweetsReply$text)
+
+rawTweetsPos <- searchTwitter("to:taylorswift13:)", n=2000, lang="en", since="2016-08-01")
+# Transform tweets list into a data frame
+rawTweetsPos <- twListToDF(rawTweetsPos)
+KPPosMat <- rawTextProcess(rawTweetsPos$text)
 KPPosMat$emotion <- rep("positive",nrow(KPPosMat))
-#KPPosMat <- as.matrix(KPPosMat)
 
-rawTweetsKPNeg <- fetchTweets(query = "to:katyperry:(", num = 500, lang="en")
-KPNegMat <- rawTextProcess(rawTweetsKPNeg$text,num_remove=FALSE,keywords_remove=TRUE,keywords = c("katy","perry","katyperry"), output=0)
+rawTweetsNeg <- searchTwitter("to:taylorswift13:(", n=2000, lang="en", since="2016-08-01")
+# Transform tweets list into a data frame
+rawTweetsNeg <- twListToDF(rawTweetsNeg)
+KPNegMat <- rawTextProcess(rawTweetsNeg$text)
 KPNegMat$emotion <- rep("negative",nrow(KPNegMat))
-#KPNegMat <- as.matrix(KPNegMat)
-
+ 
 #KPMixMat <- rbind(KPPosMat,KPNegMat)
-KPMixMat <- rbind(KPPosMat[1:200,],KPNegMat[1:200,])
+KPMixMat <- rbind(KPPosMat[1:2000,],KPNegMat[1:2000,])
 #library(e1071)
 #classifier = naiveBayes(KPMixMat, as.factor(KPMixMat[,2]) )
 #predicted = predict(classifier, KPMixMat[11:15,])
